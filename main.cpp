d44472fc65f3f54949fe48ac8d838c9bf53c0ada
StoriEthanTan <ethan.tan@storicard.com>
2024-02-27 23:35:57 +0800

    init this project

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by 谭天一 on 2024/2/27.
+//
+
+#include "main.h"
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World" << std::endl;
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by 谭天一 on 2024/2/27.
+//
+
+#ifndef CPPPLAYGROUND_MAIN_H
+#define CPPPLAYGROUND_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //CPPPLAYGROUND_MAIN_H
